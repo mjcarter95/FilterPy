fix: stable calculation of log likelihood

examples/pf_linear_gaussian_ssm.py
@@ -39,6 +39,7 @@ pf = BasicParticleFilter(N, transition_model, measurement_model)
 x_sim = np.zeros((T + 1, N, 1))
 y_sim = np.zeros((T + 1, N, 1))
 logw = np.zeros((T + 1, N))
+log_likelihood = np.zeros((T, N))
 
 # Set the initial state and state covariance
 x_sim[0, :, :] = np.random.multivariate_normal(np.zeros(1), np.eye(1), N)
@@ -49,7 +50,7 @@ logw[0, :] = measurement_model.lpdf(x_sim[0], y[0])
 # Run the Particle filter
 for t in tqdm(range(0, T)):
     x_pred, y_pred = pf.predict(x_sim[t])
-    x_sim[t+1], y_sim[t+1], logw[t+1] = pf.update(x_pred, y_pred, logw[t], y[t])
+    x_sim[t+1], y_sim[t+1], logw[t+1], log_likelihood[t] = pf.update(x_pred, y_pred, logw[t], y[t])
 
 # Estimate the state and observation sequences
 x_hat = np.zeros((T, 1))

filterpy/models/linear_gaussian_ssm.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.stats as stats
 
 
 class TransitionModel():
@@ -28,8 +29,8 @@ class MeasurementModel():
         lpdf = np.zeros(x.shape[0])
 
         for i in range(x.shape[0]):
-            lpdf[i] = -0.5 * np.log(2 * np.pi * self.cov) - 0.5 * (y - self.mean * x[i]) ** 2 / self.cov
-        
+            lpdf[i] = stats.multivariate_normal.logpdf(y, self.mean @ x[i], self.cov)
+
         return lpdf
 
 

filterpy/pf/basic.py
@@ -45,12 +45,11 @@ class BasicParticleFilter(BaseParticleFilter):
         # Compute the weights
         logw_new = logw + self.measurement_model.lpdf(x_pred, y)
 
+        # Calculate the log likelihood
+        log_likelihood = np.max(logw_new) + np.log(np.sum(np.exp(logw_new - np.max(logw_new))))
+
         # Normalise the weights
-        indices = ~np.isneginf(logw)
-        logw[indices] = logw[indices] - np.max(logw[indices])
-        log_likelihood = np.sum(np.exp(logw))
-        weights = np.exp(logw)
-        wn = weights / np.sum(weights)
+        wn = np.exp(logw_new - log_likelihood)
 
         # Compute the effective sample size
         ess = 1 / np.sum(wn ** 2)
@@ -66,7 +65,7 @@ class BasicParticleFilter(BaseParticleFilter):
             x_new = x_pred.copy()
             y_new = y_pred.copy()
 
-        return x_new, y_new, logw_new
+        return x_new, y_new, logw_new, log_likelihood
 
 
 